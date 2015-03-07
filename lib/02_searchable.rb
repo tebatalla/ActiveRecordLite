58fc256e92c69e052be7b0bf5f6551ed1e5b9c7c
Tomas Batalla <tebatalla@gmail.com>
2015-03-07 08:25:43 -0800

    Where to stack lazily

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -4,19 +4,53 @@ require_relative '01_sql_object'
 module Searchable
   def where(params)
     # ...
-    where_line = params.keys.map{ |k| "#{k} = ?" }.join(' AND ')
-    self.parse_all(DBConnection.execute(<<-SQL,*params.values))
-    SELECT
-      *
-    FROM
-      #{self.table_name}
-    WHERE
-      #{where_line}
-    SQL
+    Relation.new(self).where(params).evaluate
   end
+
+  # def includes(params)
+  #   Relation.new(self.parse_all(DBConnection.execute(<<-SQL,other_table)))
+  #   SELECT
+  #     *
+  #   FROM
+  #     #{self.table_name}
+  #   JOIN
+  #     #{other_table}
+  #   ON
+  #     #{self}
+  #
+  #   SQL
+  # end
 end
 
 class SQLObject
   # Mixin Searchable here...
   extend Searchable
+end
+
+class Relation
+  extend Searchable
+  def criteria
+    @criteria ||= {:conditions => {}}
+  end
+
+  def where(args)
+    criteria[:conditions].merge!(args)
+    self
+  end
+
+  def initialize(klass)
+    @klass = klass
+  end
+
+  def evaluate
+    where_line = criteria[:conditions].keys.map{ |k| "#{k} = ?" }.join(' AND ')
+    @klass.parse_all(DBConnection.execute(<<-SQL,*criteria[:conditions].values))
+    SELECT
+      *
+    FROM
+      #{@klass.table_name}
+    WHERE
+      #{where_line}
+    SQL
+  end
 end
--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -25,3 +25,24 @@ module Associatable
     end
   end
 end
+
+class Cat < SQLObject
+  belongs_to :human, foreign_key: :owner_id
+
+  finalize!
+end
+
+class Human < SQLObject
+  self.table_name = 'humans'
+
+  has_many :cats, foreign_key: :owner_id
+  belongs_to :house
+
+  finalize!
+end
+
+class House < SQLObject
+  has_many :humans
+
+  finalize!
+end
